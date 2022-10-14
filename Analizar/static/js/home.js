0ee37a081fd3ab0b92cf547092946403551636d4
DiegolPl <diegopdl1992@gmail.com>
2022-10-14 16:18:39 -0300

    Agrego tabla de mas info e interactividad

--- Analizar/static/js/home.js
+++ Analizar/static/js/home.js
@@ -6,3 +6,14 @@ for(let i = 0; i < links.length; i++){
         links[i].classList.add('link-tabla-datos-activo')
     })
 }
+
+let btnOpenTabla = document.getElementById('open-tabla');
+
+btnOpenTabla.addEventListener('click', ()=>{
+    document.getElementById('tabla').classList.add('d-block');
+})
+let btnCloseTabla = document.getElementById('close-tabla');
+
+btnCloseTabla.addEventListener('click', ()=>{
+    document.getElementById('tabla').classList.remove('d-block');
+})
