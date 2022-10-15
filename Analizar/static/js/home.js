5e59d9164c67fbf468e725d4be143ca599f7c663
DiegolPl <diegopdl1992@gmail.com>
2022-10-14 23:09:03 -0300

    Agrego modo edit de precio

--- Analizar/static/js/home.js
+++ Analizar/static/js/home.js
@@ -17,3 +17,29 @@ let btnCloseTabla = document.getElementById('close-tabla');
 btnCloseTabla.addEventListener('click', ()=>{
     document.getElementById('tabla').classList.remove('d-block');
 })
+
+let btnOpenEdit = document.getElementById('btn-open-edit-precio');
+btnOpenEdit.addEventListener('click', ()=>{
+    document.getElementById('form-edit-precio').classList.add('d-flex');
+})
+
+let btnCloseEdit = document.getElementById('btn-close-edit-precio');
+btnCloseEdit.addEventListener('click', ()=>{
+    document.getElementById('form-edit-precio').classList.remove('d-flex');
+})
+
+// localStorage.setItem('costo-luz','130');
+
+let precioEnMemoria = Number(sessionStorage.getItem('costo-luz'))
+
+function cargarDatos() {
+    document.getElementById('input-precio').setAttribute('value',precioEnMemoria); 
+}
+
+document.getElementById('btn-save-edit-precio').addEventListener('click',guardarDato);
+
+function guardarDato(){
+    let nuevoPrecio = document.getElementById('input-precio').getAttribute('value');
+    sessionStorage.setItem('costo-luz',nuevoPrecio);
+    document.getElementById('form-edit-precio').classList.remove('d-flex');
+}
