3d9ce675f8bbcb0a97a297b47a9436a82f7e36b1
DiegolPl <diegopdl1992@gmail.com>
2022-11-08 12:57:26 -0300

    Arreglo bug del boton del menu

--- Analizar/static/js/effects.js
+++ Analizar/static/js/effects.js
@@ -1,11 +1,11 @@
 //Cuando se presione sobre el icono del menu despliega un menu
-isClosed = false;
+isClosed = true;
 menuLateral.addEventListener('click',() => {
     hambugerChange();
 });
 hambugerChange = () => {
     // Si esta cerrado el menu, saco el icono del menu hamburgesa y despliego el menu
-    if (isClosed == true) {          
+    if (isClosed === true) {          
         changeIcon.classList.remove('bi', 'bi-list');
         changeIcon.classList.add('bi', 'bi-x-lg');
         //submenu.classList.remove('d-none');
