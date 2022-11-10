7f61b526833f37c6fc24651db98ae3e70e5ff506
DiegolPl <diegopdl1992@gmail.com>
2022-11-10 14:56:36 -0300

    Borro codigos comentados que no son mas necesarios

--- Analizar/static/js/notificaciones.js
+++ Analizar/static/js/notificaciones.js
@@ -46,15 +46,6 @@ cerrarFormularioNuevaAlerta.addEventListener('click', ()=>{
     contenedorAlertas.classList.remove('d-none')
 })
 
-// for(let i = 0; i < btnsEditAlerta.length; i++){
-//     btnsEditAlerta[i].addEventListener('click',()=>{
-//         contenedorAlertas.classList.add('d-none')
-//         contenedorAlertas.classList.remove('d-grid')
-//         formularioEditarAlerta.classList.add('d-flex')
-//         formularioEditarAlerta.classList.remove('d-none')
-//     })
-// }
-
 cerrarFormularioEditarAlerta.addEventListener('click',()=>{
     contenedorAlertas.classList.add('d-grid')
     contenedorAlertas.classList.remove('d-none')
@@ -86,7 +77,6 @@ btnGuardarNueva.addEventListener('click',()=>{
     let idAlerta = `alerta${nroAlerta}`
     nroAlerta += 1
 
-    // console.log(idAlerta)
     alertas[idAlerta] = {}
 
     alertas[idAlerta].codigo = idAlerta
@@ -140,7 +130,6 @@ function showNewAlert(id){
     contenedorBtnAlerta.appendChild(btnEliminarAlerta)
 
     btnEditarAlerta.addEventListener('click',()=>{
-        // openEditAlert(alerta)
         contenedorAlertas.classList.add('d-none')
         contenedorAlertas.classList.remove('d-grid')
         formularioEditarAlerta.classList.add('d-flex')
@@ -150,18 +139,13 @@ function showNewAlert(id){
         let optionsSelectEdit = document.querySelectorAll('#tipo-alerta-edit > option');
         optionsSelectEdit.forEach(el => {
             if(el.value === alertaSelect.tipoAlerta.toLowerCase()){
-                // console.log(el.getAttribute().name)
                 el.setAttribute('selected',true)
-
-                // let selected = document.createAttribute('selected')
                 el.setAttributeNode(el.getAttribute('selected'))
             }else{
                 el.removeAttribute('selected')
             }
             
         })
-        
-        // document.getElementById('tipo-alerta-edit').value = alertaSelect.tipoAlerta
     })
 
     btnEliminarAlerta.addEventListener('click',()=>{
@@ -205,7 +189,6 @@ function showAlertas(){
         contenedorBtnAlerta.appendChild(btnEliminarAlerta)
 
         btnEditarAlerta.addEventListener('click',()=>{
-            // openEditAlert(alerta)
             contenedorAlertas.classList.add('d-none')
             contenedorAlertas.classList.remove('d-grid')
             formularioEditarAlerta.classList.add('d-flex')
@@ -215,18 +198,12 @@ function showAlertas(){
             let optionsSelectEdit = document.querySelectorAll('#tipo-alerta-edit > option');
             optionsSelectEdit.forEach(el => {
                 if(el.value === alertaSelect.tipoAlerta.toLowerCase()){
-                    // console.log(el.getAttribute().name)
                     el.setAttribute('selected',true)
-
-                    // let selected = document.createAttribute('selected')
                     el.setAttributeNode(el.getAttribute('selected'))
                 }else{
                     el.removeAttribute('selected')
                 }
-                
             })
-            
-            // document.getElementById('tipo-alerta-edit').value = alertaSelect.tipoAlerta
         })
 
         btnEliminarAlerta.addEventListener('click',()=>{
@@ -236,136 +213,3 @@ function showAlertas(){
 
 }
 showAlertas()
-
-//Generar el formulario mediante una funcion que tome un id para poder vincularlo.. esa id db estar een el AEL como parametro
-// function openEditAlert(id){
-    
-//     //Form
-//     let formEdit = document.createElement('FORM')
-//     formEdit.classList.add('formulario-nueva-alerta')
-//     formEdit.setAttribute('id','form-edit-alerta')
-//     contenedorPrincipalAlertas.appendChild(formEdit)
-
-//     //P
-//     let tituloFormEdit = document.createElement('P')
-//     tituloFormEdit.classList.add('title-formulario-alerta')
-//     tituloFormEdit.textContent = 'Modo editar alerta!'
-//     formEdit.appendChild(tituloFormEdit)
-
-//     //Div
-//     let divContInputFormEdit = document.createElement('DIV')
-//     divContInputFormEdit.classList.add('inputContainer')
-//     formEdit.appendChild(divContInputFormEdit)
-
-//     //Input
-//     let inputCantidadFormEdit = document.createElement('INPUT')
-//     inputCantidadFormEdit.classList.add('input')
-//     inputCantidadFormEdit.setAttribute('type','number')
-//     inputCantidadFormEdit.setAttribute('placeholder','Ingresa la cantidad de w/h')
-//     inputCantidadFormEdit.setAttribute('name','cantidad')
-//     inputCantidadFormEdit.setAttribute('value',alertas[id].cantidad)
-//     inputCantidadFormEdit.setAttribute('id',`input-precio-${id}`)
-//     divContInputFormEdit.appendChild(inputCantidadFormEdit)
-//     document.getElementById(`input-precio-${id}`).value = alertas[id].cantidad
-
-//     //Label
-//     let labelCantidadFormEdit = document.createElement('LABEL')
-//     labelCantidadFormEdit.classList.add('label')
-//     labelCantidadFormEdit.setAttribute('for','input-precio')
-//     labelCantidadFormEdit.textContent = 'Cantidad w/h'
-//     divContInputFormEdit.appendChild(labelCantidadFormEdit)
-
-//     //Div
-//     let divContSelectFormEdit = document.createElement('DIV')
-//     divContSelectFormEdit.classList.add('inputContainer')
-//     formEdit.appendChild(divContSelectFormEdit)
-
-//     //Select
-//     let selectFormEdit = document.createElement('SELECT')
-//     selectFormEdit.classList.add('tipo-alerta')
-//     selectFormEdit.setAttribute('name','tipo-alerta')
-//     selectFormEdit.setAttribute('id','tipo-alerta')
-//     divContSelectFormEdit.appendChild(selectFormEdit)
-
-//     //Option1
-//     let option1FormEdit = document.createElement('OPTION')
-//     option1FormEdit.setAttribute('selected','selected')
-//     option1FormEdit.setAttribute('disabled','disabled')
-//     option1FormEdit.textContent = 'Elije el tipo de alerta'
-//     selectFormEdit.appendChild(option1FormEdit)
-
-//     //Option2
-//     let option2FormEdit = document.createElement('OPTION')
-//     option2FormEdit.setAttribute('value','diario')
-//     option2FormEdit.textContent = 'Diario'
-//     selectFormEdit.appendChild(option2FormEdit)
-
-//     //Option3
-//     let option3FormEdit = document.createElement('OPTION')
-//     option3FormEdit.setAttribute('value','semanal')
-//     option3FormEdit.textContent = 'Semanal'
-//     selectFormEdit.appendChild(option3FormEdit)
-
-//     //Option4
-//     let option4FormEdit = document.createElement('OPTION')
-//     option4FormEdit.setAttribute('value','mensual')
-//     option4FormEdit.textContent = 'Mensual'
-//     selectFormEdit.appendChild(option4FormEdit)
-
-//     //Option5
-//     let option5FormEdit = document.createElement('OPTION')
-//     option5FormEdit.setAttribute('value','anual')
-//     option5FormEdit.textContent = 'Anual'
-//     selectFormEdit.appendChild(option5FormEdit)
-
-//     //Div
-//     let divContBtnFormEdit = document.createElement('DIV')
-//     divContBtnFormEdit.classList.add('btnContainer')
-//     formEdit.appendChild(divContBtnFormEdit)
-
-//     //Boton editar
-//     let btnEditarFormEdit = document.createElement('BUTTON')
-//     btnEditarFormEdit.classList.add('submitBtn')
-//     btnEditarFormEdit.classList.add('saveBtn')
-//     btnEditarFormEdit.setAttribute('type','button')
-//     btnEditarFormEdit.setAttribute('id','btn-save-edit-alerta')
-//     btnEditarFormEdit.textContent = 'Guardar'
-//     divContBtnFormEdit.appendChild(btnEditarFormEdit)
-
-//     //Boton Cancelar
-//     let btnCancelarFormEdit = document.createElement('BUTTON')
-//     btnCancelarFormEdit.classList.add('submitBtn')
-//     btnCancelarFormEdit.classList.add('cancelBtn')
-//     btnCancelarFormEdit.setAttribute('type','button')
-//     btnCancelarFormEdit.setAttribute('id','btn-close-edit-alerta')
-//     btnCancelarFormEdit.textContent = 'Cancelar'
-//     divContBtnFormEdit.appendChild(btnCancelarFormEdit)
-
-//     //Generar la vista del formulario
-//     contenedorAlertas.classList.add('d-none')
-//     contenedorAlertas.classList.remove('d-grid')
-//     formEdit.classList.add('d-flex')
-//     formEdit.classList.remove('d-none')
-
-//     //Funcionalidad al boton guardar
-//     btnEditarFormEdit.addEventListener('click',()=>{
-//         alert(document.getElementById(`input-precio-${id}`).value)
-//         alertas[id].cantidad = document.getElementById(`input-precio-${id}`).value
-//         document.getElementById(`texto-alerta-${id}`).textContent = `Si el consmo supera los ${alertas[id].cantidad} w/h envio una alerta como precaucion`
-
-//         //Cuando salgo me deja de andar, creo que debo usar el form del html y tratar de vincularlo con cada boton de alguna forma
-
-//         formEdit.classList.add('d-none')
-//         formEdit.classList.remove('d-flex')
-//         contenedorAlertas.classList.add('d-grid')
-//         contenedorAlertas.classList.remove('d-none')
-//     })
-
-//     //Funcionalidad al boton cancelar
-//     btnCancelarFormEdit.addEventListener('click', ()=>{
-//         formEdit.classList.add('d-none')
-//         formEdit.classList.remove('d-flex')
-//         contenedorAlertas.classList.add('d-grid')
-//         contenedorAlertas.classList.remove('d-none')
-//     })
-// }
