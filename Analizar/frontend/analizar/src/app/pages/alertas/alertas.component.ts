105802ab0f3806f0545eefc0d4a728b113aa69da
Frangia22 <giacherofranc22@gmail.com>
2023-04-28 08:45:00 -0300

    Creo componente alertas

---
+++ Analizar/frontend/analizar/src/app/pages/alertas/alertas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alertas',
+  templateUrl: './alertas.component.html',
+  styleUrls: ['./alertas.component.css']
+})
+export class AlertasComponent {
+
+}
--- Analizar/frontend/analizar/src/app/pages/pages.module.ts
+++ Analizar/frontend/analizar/src/app/pages/pages.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from '../app-routing.module';
 import { Pagina404Component } from './pagina404/pagina404.component';
+import { AlertasComponent } from './alertas/alertas.component';
 
 
 @NgModule({
   declarations: [
     //Aca deben declarar los componentes de pages que crearon
-  
-    Pagina404Component
+    Pagina404Component,
+    AlertasComponent
   ],
   imports: [
     CommonModule,
@@ -16,7 +17,8 @@ import { Pagina404Component } from './pagina404/pagina404.component';
   ],
   exports: [
     //Aca deben exportar los componentes de pages creados, para ser utilizados por otros modulos
-    Pagina404Component
+    Pagina404Component,
+    AlertasComponent
   ]
 })
 export class PagesModule { }
