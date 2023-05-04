b16cd5174992cb3acc525eac54ca5b474e2b9d7f
DiegolPl <diegopdl1992@gmail.com>
2023-05-04 15:35:50 -0300

    Agrego vista dashboard del cliente

--- Analizar/frontend/analizar/src/app/app-routing.module.ts
+++ Analizar/frontend/analizar/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AlertasComponent } from './pages/alertas/alertas.component';
 import { Pagina404Component } from './pages/pagina404/pagina404.component';
+import { DashboardClienteComponent } from './pages/dashboard-cliente/dashboard-cliente.component';
 
 const routes: Routes = [
   //Definir las rutas de la app
   { path: 'alertas', component: AlertasComponent},
+  { path: 'home', component: DashboardClienteComponent},
   { path: '**', component: Pagina404Component}
 ];
 
--- Analizar/frontend/analizar/src/app/pages/pages.module.ts
+++ Analizar/frontend/analizar/src/app/pages/pages.module.ts
@@ -4,12 +4,14 @@ import { AppRoutingModule } from '../app-routing.module';
 import { Pagina404Component } from './pagina404/pagina404.component';
 import { AlertasComponent } from './alertas/alertas.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DashboardClienteComponent } from './dashboard-cliente/dashboard-cliente.component';
 
 @NgModule({
   declarations: [
     //Aca deben declarar los componentes de pages que crearon
     Pagina404Component,
-    AlertasComponent
+    AlertasComponent,
+    DashboardClienteComponent
   ],
   imports: [
     CommonModule,
