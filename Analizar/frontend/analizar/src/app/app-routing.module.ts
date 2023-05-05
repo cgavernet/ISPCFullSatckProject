b33ceaf413fa09fee3d877acbf1fe484ca28e8aa
Frangia22 <giacherofranc22@gmail.com>
2023-05-05 08:59:14 -0300

    Implemento el service creado en las distintas view

--- Analizar/frontend/analizar/src/app/app-routing.module.ts
+++ Analizar/frontend/analizar/src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { AlertasComponent } from './pages/alertas/alertas.component';
 import { LoginComponent } from './pages/login/login.component';
 import { Pagina404Component } from './pages/pagina404/pagina404.component';
+import { AuthGuard } from './service/authGuard.service';
 
 const routes: Routes = [
   //Definir las rutas de la app
-  { path: 'alertas', component: AlertasComponent},
+  { path: 'alertas', component: AlertasComponent, canActivate: [AuthGuard]},
   { path: 'login', component: LoginComponent},
   { path: '**', component: Pagina404Component}
 ];
