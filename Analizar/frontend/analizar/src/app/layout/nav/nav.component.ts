35b1123df532c927b7f7e2d3e1b31a76d5a9703f
Frangia22 <giacherofranc22@gmail.com>
2023-05-05 23:59:16 -0300

    Fix a los submenus

--- Analizar/frontend/analizar/src/app/layout/nav/nav.component.ts
+++ Analizar/frontend/analizar/src/app/layout/nav/nav.component.ts
@@ -20,5 +20,25 @@ export class NavComponent {
     this.authService.logout();
     this.router.navigate(['/login']);
   }
+  isClosed: boolean = true;
+  openSidebar() {
+    const submenu = document.querySelector('#submenu');
+    const footer = document.querySelector('footer');
+    const changeIcon = document.querySelector('#changeIcon');
+    if (this.isClosed === true) {          
+      changeIcon?.classList.remove('bi', 'bi-list');
+      changeIcon?.classList.add('bi', 'bi-x-lg');
+      submenu?.classList.add('sidebar-activo');
+      console.log('Abro menu');
+      this.isClosed = false;
+      // Si esta abierto el menu, saco el icono de la cruz y cierro el menu
+    } else {   
+      changeIcon?.classList.remove('bi', 'bi-x-lg');
+      changeIcon?.classList.add('bi', 'bi-list');
+      submenu?.classList.remove('sidebar-activo');
+      console.log('Cierro menu');
+      this.isClosed = true;
+    }
+  }
 
 }
