434a49799f50544e5c8f2209d738244ff14bf267
Frangia22 <giacherofranc22@gmail.com>
2023-05-17 23:06:48 -0300

    modifico vista alerta

--- Analizar/frontend/analizar/src/app/pages/alertas/alertas.component.ts
+++ Analizar/frontend/analizar/src/app/pages/alertas/alertas.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { NgModel } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, NgModel, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AlertasService } from 'src/app/service/alertas.service';
 import { Alert } from './Alert';
 
 
@@ -9,14 +10,42 @@ import { Alert } from './Alert';
   templateUrl: './alertas.component.html',
   styleUrls: ['./alertas.component.css']
 })
-export class AlertasComponent {
+export class AlertasComponent implements OnInit {
 
-constructor(private router: Router) {}
+constructor(private router: Router, private alertaService: AlertasService, private fb: FormBuilder) {}
  alerts: Alert[] = [];
- setAlert!: number;
- typeAlert!: string;
+ valor!: number;
+ medidor!: string;
+ fechaAlta!: Date;
+ alertas: any[] = [];
+ alertasForm!: FormGroup;
+ loginError: string = '';
+ 
+ ngOnInit(): void{
+  this.alertasForm = this.initForm()
+  this.alertaService.getAlertas().subscribe((data: any) => {
+    this.alertas = data   
+  })
+  
+ }
+   //Validaciones para los campos
+   
 
  saveAlert(){
+  const valor = this.valor; 
+  const typeAlert = this.medidor;
+  if(this.alertasForm.valid){
+  this.alertaService.addAlertas(valor, typeAlert).subscribe((alert: any) => {
+    console.log('Alerta agregada con éxito:', alert);
+    this.closeModal();
+  }, (error: any) => {
+    console.error('Hubo un error al agregar la alerta', error);
+  })
+  }else{
+    this.alertasForm.markAllAsTouched();
+    this.loginError = 'Complete los campos';
+  }
+  /*
   if(this.setAlert && this.typeAlert){
     let alert = new Alert();
     console.log(alert.setAlert);
@@ -30,7 +59,7 @@ constructor(private router: Router) {}
     this.closeModal();
   }else{
     alert("Ingrese una alerta");
-  }
+  }*/
 
  }
  updateAlert(){}
@@ -70,6 +99,12 @@ constructor(private router: Router) {}
     if(contenedorAlertas != null) {
       contenedorAlertas.style.display ='grid';
     }
+  }
+  initForm(): FormGroup {
+    return this.fb.group({
+      valor: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+      medidor: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+    })
   }
 
 }
