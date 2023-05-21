bdc0f27447b37e5c959cfea305f6b8e928d6e803
Frangia22 <giacherofranc22@gmail.com>
2023-05-21 18:44:18 -0300

    Migrations de modelos

--- Analizar/backend/analizar/consumos/urls.py
+++ Analizar/backend/analizar/consumos/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import getConsumos
 
 urlpatterns = [
-    path('consumos/', getConsumos, name= 'getConsumos')
+    path('', getConsumos, name= 'getConsumos')
 ]
