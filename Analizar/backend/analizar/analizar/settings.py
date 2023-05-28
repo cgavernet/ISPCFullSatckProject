188dbb2bb78c9417e94362d45108ad5d51291ec7
DiegolPl <diegopdl1992@gmail.com>
2023-05-28 16:11:16 -0300

    Genero conexion entre front y back de consumo

--- Analizar/backend/analizar/analizar/settings.py
+++ Analizar/backend/analizar/analizar/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -52,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware"
 ]
 
 ROOT_URLCONF = 'analizar.urls'
@@ -126,3 +129,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:4200',
+]
--- Analizar/frontend/analizar/src/app/service/consumo.service.ts
+++ Analizar/frontend/analizar/src/app/service/consumo.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class ConsumoService {
 
-  private apiServerUrl = "http://localhost:3000/consumos"
+  private apiServerUrl = "http://127.0.0.1:8000/consumos/"
 
   constructor( private http: HttpClient) { }
 
