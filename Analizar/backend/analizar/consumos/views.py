c175c9d070884214f3af616fcd9363965ea45ffb
DiegolPl <diegopdl1992@gmail.com>
2023-05-28 15:36:07 -0300

    Agrego cambios en consumos y los metodos GET y POST

--- Analizar/backend/analizar/consumos/views.py
+++ Analizar/backend/analizar/consumos/views.py
@@ -1,9 +1,16 @@
 from django.core import serializers
 from django.http import JsonResponse
 from .models import Consumos
+import json
 
 # Create your views here.
 def getConsumos(request):
-    consumos = Consumos.objects.all()
-    data = serializers.serialize('json', consumos)
+    consumos = list(Consumos.objects.values())
+    data = consumos
     return JsonResponse(data, safe=False)
+
+def postConsumos(request):
+    jd = json.loads(request.body)
+    Consumos.objects.create(medidor=jd["id_medidor"],fechaMedicion=jd["fecha_medicion"],consumo=jd["consumo"])
+    datos = {'message': "success"}
+    return JsonResponse(datos)
